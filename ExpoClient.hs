fd26a361daccfe821e1eb681b96eb234d7ada380
Daniel Stankevich <danstn@users.noreply.github.com>
2020-04-02 16:13:39 +1100

    Push ticket response decoder (#2)
    * Update README.md
    
    * Add push ticket response decoder
    
    * Update README.md

--- ExpoClient.hs
+++ ExpoClient.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TemplateHaskell            #-}
 
 module ExpoClient
@@ -11,8 +16,11 @@ module ExpoClient
   , createMessageSimple
   ) where
 
+import           GHC.Exts
+
 import           Data.Aeson
 import           Data.Aeson.TH
+import           Data.Aeson.Types
 import           Data.ByteString.Lazy.Char8 (ByteString)
 import           Data.Text
 
@@ -21,17 +29,8 @@ import           Network.HTTP.Req
 expoHost :: Text
 expoHost = "exp.host"
 
--- | Types
--- -----------------------------------------------------------------------------
-
-newtype PushToken = PushToken Text
-newtype Title = Title Text
-newtype Body = Body Text
-newtype Ttl = Ttl Integer
-newtype Expiration = Expiration Integer
-newtype Badge = Badge Integer
-data Sound = SoundDefault
-data Priority = PriorityDefault | PriorityNormal | PriorityHigh
+-- | Push Message
+--------------------------------------------------------------------------------
 
 data PushMessage = PushMessage
   { _to         :: PushToken
@@ -45,8 +44,23 @@ data PushMessage = PushMessage
   , _badge      :: Maybe Badge
   }
 
--- | Codecs
--- -----------------------------------------------------------------------------
+newtype PushToken = PushToken Text
+
+newtype Title = Title Text
+
+newtype Body = Body Text
+
+newtype Ttl = Ttl Integer
+
+newtype Expiration = Expiration Integer
+
+newtype Badge = Badge Integer
+
+data Sound = SoundDefault
+
+data Priority = PriorityDefault
+              | PriorityNormal
+              | PriorityHigh
 
 instance ToJSON Sound where
   toJSON SoundDefault = "default"
@@ -56,6 +70,104 @@ instance ToJSON Priority where
   toJSON PriorityNormal  = "normal"
   toJSON PriorityHigh    = "high"
 
+
+-- | Push Ticket
+--------------------------------------------------------------------------------
+
+data PushTicket = PushTicket
+  { _status       :: Status
+  , _ticketId     :: Maybe TicketId
+  , _error        :: Maybe Error
+  , _errorMessage :: Maybe Text
+  } deriving Show
+
+instance FromJSON PushTicket where
+  parseJSON = withObject "PushTicket" $ \o -> do
+    _status   <- o .: "status"
+    _ticketId <- o .:? "id"
+    _error    <- o .:? "error"
+    details   <- o .:? "details"
+    _errorMessage <-
+      case details of
+        Just v  -> v .: "error"
+        Nothing -> pure Nothing
+    pure PushTicket{..}
+
+data Status = StatusOk
+            | StatusError
+            deriving (Show)
+
+instance FromJSON Status where
+  parseJSON v@(String s) = case s of
+    "ok"    -> pure StatusOk
+    "error" -> pure StatusError
+    _       -> unexpected v
+  parseJSON v = typeMismatch "String" v
+
+newtype TicketId = TicketId Text
+  deriving (Show, FromJSON)
+
+data Error = DeviceNotRegistered
+           | MessageTooBig
+           | MessageRateExceeded
+           | InvalidCredentials
+           | UnknownError Text
+           deriving (Show)
+
+instance FromJSON Error where
+  parseJSON = withText "Error" $ pure . \case
+    "DeviceNotRegistered" -> DeviceNotRegistered
+    "MessageTooBig"       -> MessageTooBig
+    "MessageRateExceeded" -> MessageRateExceeded
+    "InvalidCredentials"  -> InvalidCredentials
+    s                     -> UnknownError s
+
+-- | Push ticket response format
+-- https://docs.expo.io/versions/latest/guides/push-notifications/#push-ticket-format
+-- {
+--   "data": [
+--     {
+--       "status": "error" | "ok",
+--       "id": string, // this is the Receipt ID
+--       // if status === "error"
+--       "message": string,
+--       "details": JSON
+--     },
+--     ...
+--   ],
+--   // only populated if there was an error with the entire request
+--   "errors": [{
+--     "code": number,
+--     "message": string
+--   }]
+-- }
+--------------------------------------------------------------------------------
+data PushTicketResponse = PushTicketResponse
+  { _responseData   :: [PushTicket]
+  , _responseErrors :: [Error]
+  } deriving Show
+
+instance FromJSON PushTicketResponse where
+  parseJSON = withObject "PushTicketResponse" $ \o -> do
+    v <- o .: "data"
+    _responseData <- case v of
+      Array _  -> parseJSON  v
+      Object _ -> parseJSON $ Array $ fromList [v]
+      _        -> unexpected v
+    _responseErrors <- o .:? "errors" .!= []
+    pure PushTicketResponse{..}
+
+data RequestError = RequestError
+  { _errCode :: Integer
+  , _errMsg  :: Text
+  } deriving Show
+
+instance FromJSON RequestError where
+  parseJSON = withObject "RequestError" $ \o ->
+    RequestError <$> o .: "code" <*> o .: "message"
+
+-- | Generic Codecs
+-- -----------------------------------------------------------------------------
 
 $(deriveToJSON defaultOptions ''PushToken)
 $(deriveToJSON defaultOptions ''Title)
@@ -71,8 +183,8 @@ $(deriveToJSON defaultOptions
 -- | Helpers
 -- -----------------------------------------------------------------------------
 
-createMessageSimple :: PushToken -> Title -> Body -> PushMessage
-createMessageSimple to' title' body' = PushMessage
+createMessageSimple :: Title -> Body -> PushToken -> PushMessage
+createMessageSimple title' body' to' = PushMessage
   { _to         = to'
   , _data       = Nothing
   , _title      = Just title'
@@ -87,14 +199,15 @@ createMessageSimple to' title' body' = PushMessage
 -- | Operations
 -- -----------------------------------------------------------------------------
 
-sendRequest :: ByteString -> IO ByteString
+sendRequest :: ByteString -> IO (Either String PushTicketResponse)
 sendRequest b = runReq defaultHttpConfig $ do
   let url = https expoHost /: "--" /: "api" /: "v2" /: "push" /: "send"
   let options = header "Accept" "application/json"
              <> header "Accept-Encoding" "gzip, deflate"
              <> header "Content-Type" "application/json"
-  responseBody <$> req POST url (ReqBodyLbs b) lbsResponse options
+  bs <- responseBody <$> req POST url (ReqBodyLbs b) lbsResponse options
+  pure $ eitherDecode bs
 
-sendPushNotification :: PushMessage -> IO ByteString
+sendPushNotification :: PushMessage -> IO (Either String PushTicketResponse)
 sendPushNotification = sendRequest . encode
 
